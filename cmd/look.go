00622f87558ec5a5475e5ccc9ba4b739aea855fc
Tejaswi Kasat <kasattejasvi@gmail.com>
2021-07-31 19:56:53 +0530

    Added list files

--- cmd/look.go
+++ cmd/look.go
@@ -2,35 +2,81 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 // lookCmd represents the look command
 var lookCmd = &cobra.Command{
 	Use:   "look",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Perform advanced search and replace operations",
+	Long: `Perform search and replace related operations
+Pass path using -p or --path`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("look called")
+		//Getting data of the flag passed
+		filePath, _ := cmd.Flags().GetString("path")
+		isRecursive, _ := cmd.Flags().GetBool("recursive")
+		if filePath == "" {
+			log.Fatal("Empty file path passed")
+		}
+		//	if recursive is passed, read all files under that directory and subdirectory
+		//  else list the files present only in that particular directory
+		//  else a single path is passed
+		if isDir(filePath) {
+			fmt.Println(listAllFiles(filePath, isRecursive))
+		}
+
 	},
 }
 
 func init() {
+	// Initializing root command
 	rootCmd.AddCommand(lookCmd)
 
-	// Here you will define your flags and configuration settings.
+	//Adding a flag to the command
+	lookCmd.Flags().StringP("path", "p", "", "File path in which search needs to be done")
+	lookCmd.Flags().BoolP("recursive", "r", false, "Read files recursively")
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lookCmd.PersistentFlags().String("foo", "", "A help for foo")
+//returns if passed path is a file or a directory
+func isDir(path string) bool {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fileInfo.IsDir()
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lookCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+//list files recursively
+func listAllFiles(path string, isRecursive bool) []string {
+	var fileList []string
+	l, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if isRecursive {
+		for _, file := range l {
+			if file.IsDir() {
+				recList := listAllFiles(filepath.Join(path, file.Name()), true)
+				fileList = append(fileList, recList...)
+			} else {
+				fileList = append(fileList, filepath.Join(path, file.Name()))
+			}
+		}
+	} else {
+		for _, file := range l {
+			if !file.IsDir() {
+				fileList = append(fileList, filepath.Join(path, file.Name()))
+			}
+		}
+	}
+	return fileList
+}
+
+//Read file of a given path
+func readFile(path string) {
+
 }
--- docs/look.md
+++ docs/look.md
@@ -1,9 +1,8 @@
 # look commmand
 
 ## initial steps
-1. How to read files ?
-1. Input file resursively, standard input from console.
+1. Input file recursively, standard input from console.
 1. Detect if the file is binary or text ?
 1. Divide file based on lines (LF) and read 1 line at a time in buffer.
 1. Match regex and find out the index between which the pattern is matching.
-1. Change the color of matching string and print it to con
+1. Change the color of matching string and print it to console.
